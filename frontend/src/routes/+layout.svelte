dd775b018896acc3acd9f25f2bd4d72fc055af7a
Eugene Lebedev <eugene.0x90@gmail.com>
2025-06-30 22:56:44 +0300

    Add frontend essentials

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import '../app.css';
+	import { Toaster } from '$lib/components/ui/sonner/index.js';
+	import { ModeWatcher } from 'mode-watcher';
+
+	let { children } = $props();
+</script>
+
+<Toaster position="top-right" />
+
+<ModeWatcher />
+{@render children()}
