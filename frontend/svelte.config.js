aef57cd87753a673bc0b0d9e4384ae83778c69d5
Eugene Lebedev <eugene.0x90@gmail.com>
2025-11-18 17:06:36 +0300

    Fix build issue

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -6,7 +6,11 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter({
+			fallback: 'index.html' // may differ from host to host
+		})
+	}
 };
 
 export default config;
